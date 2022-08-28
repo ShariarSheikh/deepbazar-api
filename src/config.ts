df412fa5e2b601450e978097489c0ba74e63b465
ShariarSheikh <shariar.dev@gmail.com>
2022-08-28 22:39:35 +0600

    update config

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,7 @@ export const PORT = process.env.PORT
 
 export const environment = process.env.ENVIRONMENT as string
 
-export const corsUrls = [process.env.CORS_URL_1, environment === "development" && "http://localhost:3000" ] as string[]
+export const corsUrls = [
+  process.env.CORS_URL_1,
+  environment === 'development' && 'http://localhost:3000',
+] as string[]
